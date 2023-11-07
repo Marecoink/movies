ea8553341bb4c07e296c6b54d2682612d30cd39e
Markass <marekonk@gmail.com>
2023-11-07 20:08:41 +0100

    Add Review layers with option to add review to selected movie in db

---
+++ src/main/java/com/marecoink/movies/ReviewRepository.java
@@ -0,0 +1,10 @@
+package com.marecoink.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+
+}
